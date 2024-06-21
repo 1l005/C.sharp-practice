0286db762c88d887a7d92c37d507227f1af79489
1l005 <arts10m0l5@gmail.com>
2024-06-21 21:30:30 +0500

    A2.1-A2.3
    A2.1-A2.3

---
+++ c-charp.pro/A/1l005/A/A/Program.cs
@@ -0,0 +1,72 @@
+﻿class Program
+{
+    static void Main()
+    {
+        #region A2.1. Дано двузначное число. Найти:
+        ////а) число десятков в нем;
+        ////б) число единиц в нем;
+        ////в) сумму его цифр;
+        ////г) произведение его цифр.
+
+        //Console.WriteLine("Введите двухзначное число: ");
+        //int n = int.Parse(Console.ReadLine());
+
+        //if (n < 10 || n > 99)
+        //{
+        //    Console.WriteLine("Введенное число не трёхзначное");
+        //    return;
+        //}
+
+        //int edin = n % 10;
+        //int ten = n / 10;
+        //int sum = ten + edin;
+        //int prois = ten * edin;
+
+        //Console.WriteLine($"a:{ten}, b:{edin}, c:{sum}, d:{prois}");
+
+        #endregion
+
+        #region A2.2. Дано двузначное число. Получить число, образованное при перестановке цифр заданного числа.
+
+        //Console.WriteLine("Введите двухзначное число");
+        //int n = int.Parse(Console.ReadLine());
+
+        //if (n < 10 || n > 99)
+        //{
+        //    Console.WriteLine("Введенное число не двухзначное");
+        //    return;
+        //}
+
+        //int edin = n % 10;
+        //int ten = n / 10;
+
+        //Console.WriteLine($"Полученное число:{edin}{ten}");
+
+        #endregion
+
+        #region A2.3. Дано трехзначное число. Найти:
+        ////а) число единиц в нем;
+        ////б) число десятков в нем;
+        ////в) сумму его цифр;
+        ////г) произведение его цифр.
+
+        //Console.WriteLine("Введиет трёхзначное число: ");
+        //int n = int.Parse(Console.ReadLine());
+
+        //if (n < 100 || n > 999)
+        //{
+        //    Console.WriteLine("Вы введли не трёхзначное число.");
+        //    return;
+        //}
+
+        //int edin = n % 10;
+        //int ten = (n / 10) % 10;
+        //int sotn = n / 100;
+
+        //Console.WriteLine($"Вывод: a:{edin}, b:{ten}, c:{edin+ten+sotn}, d:{edin* ten * sotn}");
+
+        #endregion
+    }
+
+
+}
